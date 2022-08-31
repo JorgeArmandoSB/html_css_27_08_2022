380e1e461b8c7d78e1ef485ae42d41b4075c2ce2
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-08-30 21:30:25 -0500

    imprimir la consola

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+let nombre =`Jorge Armando Silva Barragan`; 
+let edad = 38;
+let altura = 1.8;
+let soyEstudiante = true;
+let direcciones = null;
+let cambioRopa = undefined;
+let listaPasatiempos = ["leer" , "guitarra" , "video juegos" , "escucharmusica"];
+let caracteristicasPc = {
+    sistema: "windows 10 pro 64x",
+    ram: 12,
+    disco: "SSD", 
+    iluminacion(){
+        return "si";
+    }
+}
+
+
+let myStyle = `color: #fff;background: #b23a48;font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;border-radius: 5px;padding: 5px;`;
+console.group(`%cNombre del estudiante`, myStyle);
+    console.log(nombre);
+console.groupEnd();
+console.group(`%cEdad del estudiante`, myStyle);
+    console.log(edad);
+console.groupEnd();
+console.group(`%cAltura del estudiante`, myStyle);
+    console.log(altura);
+console.groupEnd();
+console.group(`%cPermiso para estudiar`, myStyle);
+    console.log(soyEstudiante);
+console.groupEnd();
+console.group(`%cDirrecion del estudiante`, myStyle);
+    console.log(direcciones);
+console.groupEnd();
+console.group(`%cRopa del estudianter`, myStyle);
+    console.log(cambioRopa);
+console.groupEnd();
+console.group(`%cPasatiempos del estudiante`, myStyle);
+    console.log(listaPasatiempos);
+console.groupEnd();
+console.group(`%cPC del estudiante`, myStyle);
+    console.log(caracteristicasPc);
+console.groupEnd();
+
